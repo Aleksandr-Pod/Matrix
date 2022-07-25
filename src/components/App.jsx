599a20e0bc8c4e732188b89cd776ec3cacb324f6
Aleksandr-Pod <stdfire@gmail.com>
2022-07-25 08:37:53 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,7 @@ export const App = () => {
   const sumsColumn = calcSums(data);
   const meansLine = calcMeans(data);
 
-  const showPercent = (idx) => {
+  const showPercent = (idx) => {   // Percent in row summ from total amount
     const totalSumm = sumsColumn.reduce((accum, next) => accum + next, 0);
     const calcPercent = ((sumsColumn[idx] / totalSumm) * 100).toFixed(1);
     dispatch(setPercent({ calcPercent, idx }));
@@ -33,7 +33,7 @@ export const App = () => {
             <Tsum
             onMouseEnter={() => showPercent(idxRow)}
               onMouseLeave={() => dispatch(setPercent({ calcPercent: undefined, idx: idxRow }))}>
-              {percent[idxRow] === undefined ? sumsColumn[idxRow] : `${percent[idxRow]} %`}
+              {!percent[idxRow] ? sumsColumn[idxRow] : `${percent[idxRow]} %`}
             </Tsum>
             {line.map((el, idxCol) => (   // columns
               <Tdata key={`s${idxRow}-d${idxCol}`} onClick={() => dispatch(changeData({x:idxRow, y:idxCol}))}>{el.value}</Tdata>
